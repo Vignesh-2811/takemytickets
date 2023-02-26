0c868a8d0d816604b40f69d87fefde9f35fc0928
Vignesh-2811 <shakthivignesh2002@gmail.com>
2023-02-26 13:48:08 +0530

    minor changes

--- profile.php
+++ profile.php
@@ -9,7 +9,7 @@ include "includes/aside.php";
       <h1>Profile</h1>
       <nav>
         <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item"><a href="index.php">Home</a></li>
           <li class="breadcrumb-item">Users</li>
           <li class="breadcrumb-item active">Profile</li>
         </ol>
